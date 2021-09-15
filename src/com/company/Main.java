aa61519f06ad9b85d4d1985c58c2cf9a26cfba78
Niickwan <jensen.nicklas@gmail.com>
2021-09-15 18:30:59 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void isDoubloon (String text) {
+        String textDublicate = text.toLowerCase();
+        for (int i = 0; i <= text.length() - 1; i++) {
+            int counter = 0;
+            for (int x = 0; x <= text.length() - 1; x++) {
+                if (text.toLowerCase().charAt(i) == textDublicate.toLowerCase().charAt(x)) {
+                    counter++;
+                }
+                if (counter == 3) {
+                    System.out.println("The word is not a doubloon!");
+                    return;
+                }
+            }
+            if (counter <= 1) {
+                System.out.println("The word is not a doubloon!");
+                return;
+            }
+        }
+        System.out.println("Yaaaay! The word is a doubloon!");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word to see it it's a doubloon: ");
+        String text = scanner.next();
+        isDoubloon(text);
+    }
+}
